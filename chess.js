0659a7bc4f702832d3817822060f28dbbbba3525
Paul Montgomery <finch.wren.swift@gmail.com>
2024-12-02 11:11:19 -0800

    Mon 3

--- chess.js
+++ chess.js
@@ -12,9 +12,9 @@ let pieces = {}
 function getPossibleMoveArrays(kind, color) {
     switch (kind) {
         case ROOK:
-            return [8, -8, 1, -1];
+            return [8, -8, 1, -1]
         case KNIGHT:
-            return [-17, -15, -6, 10, 17, 15, 6, -10];
+            return [-17, -15, -6, 10, 17, 15, 6, -10]
         case BISHOP:
             return [-9, -7, 9, 7]
         case QUEEN:
@@ -29,19 +29,19 @@ function getPossibleMoveArrays(kind, color) {
             }
         }
         default:
-            return []; // Return an empty array if the piece type is unrecognized
+            return [] // Return an empty array if the piece type is unrecognized
     }
 }
 
 class Cell {
     constructor(id) {
-        this.id = id;
-        this.kind = null;
-        this.color = null;
-        this.pieceId = null; 
+        this.id = id
+        this.kind = null
+        this.color = null
+        this.pieceId = null 
     }
     getId() {
-        return this.id < 10 ? `0${this.id} ` : `${this.id} `;
+        return this.id < 10 ? `0${this.id} ` : `${this.id} `
     }
 
     setPiece(piece) {
@@ -66,52 +66,52 @@ class Cell {
 class Piece {
     constructor(id, color, kind, location, limit) {
         this.id = id
-        this.color = color;
-        this.kind = kind;
-        this.setLocation(location);
-        this.limit = limit;
+        this.color = color
+        this.kind = kind
+        this.setLocation(location)
+        this.limit = limit
         this.moves = getPossibleMoveArrays(kind, color)
         // console.log( color, kind, location, limit, this.moves )
     }
     setLocation(location) {
-        this.location = parseInt(location);
+        this.location = parseInt(location)
     }
     getDetails() {
-        console.log(`My location is ${this.location} and I am a ${this.kind} color is ${this.color}`);
+        console.log(`My location is ${this.location} and I am a ${this.kind} color is ${this.color}`)
     }
 }
 
 function getPossibleMoves(movements, location, loop, limit, possibles) {
-    if (loop >= limit) return possibles;
+    if (loop >= limit) return possibles
 
     movements.forEach((movement) => {
-        let newLocation = location + movement;
+        let newLocation = location + movement
         if (newLocation >= 0 && newLocation < 64) {
-            const currentRow = Math.floor(location / 8);
-            const newRow = Math.floor(newLocation / 8);
+            const currentRow = Math.floor(location / 8)
+            const newRow = Math.floor(newLocation / 8)
 
-            if ((movement === 1 || movement === -1) && currentRow !== newRow) return;
+            if ((movement === 1 || movement === -1) && currentRow !== newRow) return
             if (
                 (movement === 9 || movement === -7) && Math.abs(currentRow - newRow) !== 1 ||
                 (movement === 7 || movement === -9) && Math.abs(currentRow - newRow) !== 1
-            ) return;
+            ) return
 
-            possibles.push(newLocation);
-            getPossibleMoves([movement], newLocation, loop + 1, limit, possibles);
+            possibles.push(newLocation)
+            getPossibleMoves([movement], newLocation, loop + 1, limit, possibles)
         }
-    });
+    })
 
-    return possibles;
+    return possibles
 }
 
 function placePiece(piece, location) {
 
-    piece.setLocation(location);
-    let possibles = [];
+    piece.setLocation(location)
+    let possibles = []
     piece.moves.forEach((move) => {
-        getPossibleMoves([move], piece.location, 0, piece.limit, possibles);
-    });
-    return possibles;
+        getPossibleMoves([move], piece.location, 0, piece.limit, possibles)
+    })
+    return possibles
 }
 
 function setup() { 
@@ -119,10 +119,48 @@ function setup() {
         let c = new Cell(i)
         board.push(c)
     }
+    pieces['rb1'] =  new Piece("rb1", BLACK, ROOK, 0, 8)
+    pieces['nb1'] =  new Piece("nb1", BLACK, KNIGHT, 1, 1)
+    pieces['bb1'] =  new Piece("bb1", BLACK, BISHOP, 2, 8)
+    pieces['bq'] =  new Piece("bq", BLACK, QUEEN, 3, 8)
+    pieces['bk'] =  new Piece("bq", BLACK, KING, 4, 8)
+    pieces['bb2'] =  new Piece("bb2", BLACK, BISHOP, 5, 8)
+    pieces['nb2'] =  new Piece("nb2", BLACK, KNIGHT, 6, 1)
+    pieces['rb2'] =  new Piece("rb2", BLACK, ROOK, 7, 8)
+    pieces['pb1'] =  new Piece("pb1", BLACK, ROOK, 8, 8)
+    pieces['pb2'] =  new Piece("pb2", BLACK, ROOK, 9, 8)
+    pieces['pb3'] =  new Piece("pb3", BLACK, ROOK, 10, 8)
+    pieces['pb4'] =  new Piece("pb4", BLACK, ROOK, 11, 8)
+    pieces['pb5'] =  new Piece("pb5", BLACK, ROOK, 12, 8)
+    pieces['pb6'] =  new Piece("pb6", BLACK, ROOK, 13, 8)
+    pieces['pb7'] =  new Piece("pb7", BLACK, ROOK, 14, 8)
+    pieces['pb8'] =  new Piece("pb8", BLACK, ROOK, 15, 8)
+    //
+    pieces['rw1'] =  new Piece("rw1", WHITE, ROOK, 56, 8)
+    pieces['nw1'] =  new Piece("nw1", WHITE, KNIGHT, 57, 1)
+    pieces['bw1'] =  new Piece("bw1", WHITE, BISHOP, 58, 8)
+    pieces['wq'] =  new Piece("wq", WHITE, QUEEN, 59, 8)
+    pieces['wk'] =  new Piece("wq", WHITE, KING, 60, 8)
+    pieces['bw2'] =  new Piece("bw2", WHITE, BISHOP, 61, 8)
+    pieces['nw2'] =  new Piece("nw2", WHITE, KNIGHT, 62, 1)
+    pieces['rw2'] =  new Piece("rw2", WHITE, ROOK, 63, 8)
+    pieces['pw1'] =  new Piece("pw1", WHITE, ROOK, 48, 8)
+    pieces['pw2'] =  new Piece("pw2", WHITE, ROOK, 49, 8)
+    pieces['pw3'] =  new Piece("pw3", WHITE, ROOK, 50, 8)
+    pieces['pw4'] =  new Piece("pw4", WHITE, ROOK, 51, 8)
+    pieces['pw5'] =  new Piece("pw5", WHITE, ROOK, 52, 8)
+    pieces['pw6'] =  new Piece("pw6", WHITE, ROOK, 53, 8)
+    pieces['pw7'] =  new Piece("pw7", WHITE, ROOK, 54, 8)
+    pieces['pw8'] =  new Piece("pw8", WHITE, ROOK, 55, 8)
+
+
 }
 
 function getBoard() { 
     return board 
+}
+function getPieces() {
+    return pieces
 }
 
 
@@ -141,5 +179,6 @@ module.exports = {
     getPossibleMoveArrays,
     setup, 
     getBoard,
-    pieces 
-};
+    pieces,
+    getPieces
+}
--- chess_test.js
+++ chess_test.js
@@ -1,18 +1,18 @@
 const {
     PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING, BLACK,WHITE, 
-    Cell, Piece, placePiece, getPossibleMoveArrays, setup, getBoard
-} = require('./chess.js');
+    Cell, Piece, placePiece, setup, getBoard, getPieces
+} = require('./chess.js')
 
 function testRook() {
-    const testRook = new Piece("r1", BLACK, ROOK, 0, 8);
-    const actual = placePiece(testRook, 20);
+    const testRook = new Piece("r1", BLACK, ROOK, 0, 8)
+    const actual = placePiece(testRook, 20)
     const expected = [28, 36, 44, 52, 60, 12, 4, 21, 22, 23, 19, 18, 17, 16
     ]
 
-    expected.sort((a, b) => b - a);
-    actual.sort((a, b) => b - a);
+    expected.sort((a, b) => b - a)
+    actual.sort((a, b) => b - a)
     const allTrue = JSON.stringify(actual) == JSON.stringify(expected)
-    const verdict = allTrue ? "PASS" : "FAIL";
+    const verdict = allTrue ? "PASS" : "FAIL"
     console.log(verdict + " testRook")
     if (allTrue == false) {
         console.log("NO!")
@@ -23,14 +23,14 @@ function testRook() {
 
 
 function testBishop() {
-    const testBishop = new Piece("bi1", BLACK, BISHOP, 0, 8);
-    const actual = placePiece(testBishop, 20);
+    const testBishop = new Piece("bi1", BLACK, BISHOP, 0, 8)
+    const actual = placePiece(testBishop, 20)
     const expected = [2, 11, 29, 38, 47, 6, 13, 27, 34, 41, 48]
-    expected.sort((a, b) => b - a);
-    actual.sort((a, b) => b - a);
+    expected.sort((a, b) => b - a)
+    actual.sort((a, b) => b - a)
     const allTrue = JSON.stringify(actual) == JSON.stringify(expected)
-    const verdict = allTrue ? "PASS" : "FAIL";
-    console.log(verdict + " testBishop");
+    const verdict = allTrue ? "PASS" : "FAIL"
+    console.log(verdict + " testBishop")
     if (allTrue == false) {
         console.log("NO!")
         console.log(actual)
@@ -40,14 +40,14 @@ function testBishop() {
 
 
 function testQueen() {
-    const testQueen = new Piece("qu1", BLACK, QUEEN, 0, 8);
-    const actual = placePiece(testQueen, 20);
+    const testQueen = new Piece("qu1", BLACK, QUEEN, 0, 8)
+    const actual = placePiece(testQueen, 20)
     const expected = [2, 11, 29, 38, 47, 6, 13, 27, 34, 41, 48, 28, 36, 44, 52, 60, 12, 4, 21, 22, 23, 19, 18, 17, 16]
-    expected.sort((a, b) => b - a);
-    actual.sort((a, b) => b - a);
+    expected.sort((a, b) => b - a)
+    actual.sort((a, b) => b - a)
     const allTrue = JSON.stringify(actual) == JSON.stringify(expected)
-    const verdict = allTrue ? "PASS" : "FAIL";
-    console.log(verdict + " testQueen");
+    const verdict = allTrue ? "PASS" : "FAIL"
+    console.log(verdict + " testQueen")
     if (allTrue == false) {
         console.log("NO!")
         console.log(actual)
@@ -57,14 +57,14 @@ function testQueen() {
 
 
 function testKing() {
-    const testKing = new Piece("king", BLACK, KING, 0, 1);
-    const actual = placePiece(testKing, 63);
+    const testKing = new Piece("king", BLACK, KING, 0, 1)
+    const actual = placePiece(testKing, 63)
     const expected = [62, 54, 55]
-    expected.sort((a, b) => b - a);
-    actual.sort((a, b) => b - a);
+    expected.sort((a, b) => b - a)
+    actual.sort((a, b) => b - a)
     const allTrue = JSON.stringify(actual) == JSON.stringify(expected)
-    const verdict = allTrue ? "PASS" : "FAIL";
-    console.log(verdict + " testKing");
+    const verdict = allTrue ? "PASS" : "FAIL"
+    console.log(verdict + " testKing")
     if (allTrue == false) {
         console.log("NO!")
         console.log(actual)
@@ -73,14 +73,14 @@ function testKing() {
 }
 
 function testKnight() {
-    const testKnight = new Piece("kn1", BLACK, KNIGHT, 0, 1);
-    const actual = placePiece(testKnight, 20);
+    const testKnight = new Piece("kn1", BLACK, KNIGHT, 0, 1)
+    const actual = placePiece(testKnight, 20)
     const expected = [37, 35, 30, 26, 14, 10, 5, 3]
-    expected.sort((a, b) => b - a);
-    actual.sort((a, b) => b - a);
+    expected.sort((a, b) => b - a)
+    actual.sort((a, b) => b - a)
     const allTrue = JSON.stringify(actual) == JSON.stringify(expected)
-    const verdict = allTrue ? "PASS" : "FAIL";
-    console.log(verdict + " testKnight");
+    const verdict = allTrue ? "PASS" : "FAIL"
+    console.log(verdict + " testKnight")
     if (allTrue == false) {
         console.log("NO!")
         console.log(actual)
@@ -90,12 +90,12 @@ function testKnight() {
 
 
 function testPawn() {
-    const testPawn = new Piece("pawn1", BLACK, PAWN, 0, 1);
+    const testPawn = new Piece("pawn1", BLACK, PAWN, 0, 1)
     const expected = [-8, -16]
-    testPawn.moves.sort((a, b) => b - a);
+    testPawn.moves.sort((a, b) => b - a)
     const allTrue = JSON.stringify(testPawn.moves) == JSON.stringify(expected)
-    const verdict = allTrue ? "PASS" : "FAIL";
-    console.log(verdict + " testPawn");
+    const verdict = allTrue ? "PASS" : "FAIL"
+    console.log(verdict + " testPawn")
     if (allTrue == false) {
         console.log("NO!")
         console.log(actual)
@@ -133,9 +133,9 @@ function testGetPossibleMoveArrays() {
         e.sort((a, b) => b - a)
         boolArray.push(JSON.stringify(p.moves) == JSON.stringify(e))
     }    
-    const isOk = boolArray.every(value => value === true);
-    const verdict = isOk ? "PASS" : "FAIL";
-    console.log(verdict + " testGetPossibleMoveArrays");
+    const isOk = boolArray.every(value => value === true)
+    const verdict = isOk ? "PASS" : "FAIL"
+    console.log(verdict + " testGetPossibleMoveArrays")
     if (isOk == false) {
         console.log("NO!")
         console.log( boolArray)
@@ -145,14 +145,23 @@ function testSetup() {
     setup() 
     const board = getBoard() 
     const isOk = board.length == 64 
-    const verdict = isOk ? "PASS" : "FAIL";
-    console.log(verdict + " testSetup");
+    const verdict = isOk ? "PASS" : "FAIL"
+    console.log(verdict + " testSetup")
+
+
  
 }
 
 
 function testBoardHasPieces() {
+    setup() 
+    const pieces = getPieces() 
+    const n = Object.keys( pieces ).length
+    const isOk = n === 32 
+    const verdict = isOk ? "PASS" : "FAIL"
+    console.log(verdict + " testPawn")
 
+    
 }
 testGetPossibleMoveArrays()
 testRook()
